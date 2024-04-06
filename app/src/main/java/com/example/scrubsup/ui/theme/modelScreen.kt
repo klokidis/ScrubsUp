2e5bf51e801c5e199bd545b2d99d092c86301eeb
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2024-04-06 20:21:18 +0300

    add model view

---
+++ app/src/main/java/com/example/scrubsup/ui/theme/modelScreen.kt
@@ -0,0 +1,56 @@
+package com.example.scrubsup.ui.theme
+
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun Screen3D(){
+    Column{
+        TopAppBar()
+        WebViewScreen()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "heart",
+                style = MaterialTheme.typography.displayLarge
+            )
+        },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun WebViewScreen() {
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                webViewClient = WebViewClient()
+                settings.domStorageEnabled = true
+            }
+        },
+        update = { webView -> //CHANGE THIS TO LOAD FROM DATA
+            webView.loadDataWithBaseURL(
+                null,
+                "<html><body style='margin:0;padding:0;'><div class=\"sketchfab-embed-wrapper\"> <iframe title=\"Realistic Human Heart\" frameborder=\"0\" allowfullscreen mozallowfullscreen=\"true\" webkitallowfullscreen=\"true\" allow=\"autoplay; fullscreen; xr-spatial-tracking\" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share width=\"360\" height=\"700\" src=\"https://sketchfab.com/models/3f8072336ce94d18b3d0d055a1ece089/embed?autostart=1&ui_hint=0&ui_theme=dark\"> </iframe> </div></body></html>",
+                "text/html",
+                "UTF-8",
+                null
+            )
+        }
+    )
+}
